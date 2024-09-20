d5ca10c82f9946e0c9614fdbaa015158b6c77a29
pro <pro@pros-MacBook-Pro.local>
2024-09-20 15:41:56 +0200

    added a cart

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react' 
+
 import './Navbar.css'
+
+import logo from '../ASSETS/Frontend_Assets/logo.png'
+
+import cart_icon from '../ASSETS/Frontend_Assets/cart_icon.png'
+
 const Navbar = () => {
   return (
-    <div>
-
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={logo} alt='logo' />
+            <p>Shopper</p>
+        </div>
+        <ul className="nav-menu">
+            <li>Shop</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button>Login</button>
+            <img src={cart_icon} alt='cart-icon' />
+        </div>
     </div>
   )
 }
