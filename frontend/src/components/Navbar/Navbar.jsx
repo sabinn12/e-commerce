5d8ba4fd0bf573030d740c53c189e89cf9caa8d3
pro <pro@pros-MacBook-Pro.local>
2024-09-21 21:24:55 +0200

    updated

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -22,6 +22,7 @@ const Navbar = () => {
         <div className="nav-login-cart">
             <button>Login</button>
             <img src={cart_icon} alt='cart-icon' />
+            <div className="nav-cart-count">0</div>
         </div>
     </div>
   )
