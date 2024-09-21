2112da53c8e391662f34be5fd55550be3b87d94e
pro <pro@pros-MacBook-Pro.local>
2024-09-21 22:56:25 +0200

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Shop from './pages/shop';
+import ShopCategory from './pages/shopcategory';
+import Product from './pages/product';
 
 function App() {
   return (
     <div >
-   <Navbar/>
+      <BrowserRouter>
+     <Navbar />
+     <Routes>
+      <Route path='/' element={<Shop/>} />
+      <Route path='/Men' element={<ShopCategory category='Men'/>} />
+      <Route path='/Women' element={<ShopCategory category='Women'/>} />
+      <Route path='/kids' element={<ShopCategory category='kids'/>} />
+      <Route path="/product" element={<Product/>}>
+      
+      </Route>
+     </Routes>
+      </BrowserRouter>
+   
     </div>
   );
 }
