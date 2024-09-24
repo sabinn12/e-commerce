646d801829b83af194da40d25df3ad3443ea9aeb
pro <pro@pros-MacBook-Pro.local>
2024-09-24 12:23:24 +0200

    updated

--- frontend/src/components/item/item.jsx
+++ frontend/src/components/item/item.jsx
@@ -5,6 +5,14 @@ const item = (props) => {
     <div className='item'>
       <img src={props.image} alt="" />
       <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">
+           {props.name_price}
+        </div>
+        <div className="item-price-old">
+          {props.old_price}
+        </div>
+      </div>
     </div>
   )
 }
