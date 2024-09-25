e815732ea6a3cb3ebce6e04c305468c021cb0041
pro <pro@pros-MacBook-Pro.local>
2024-09-25 19:15:20 +0200

    updated

--- frontend/src/pages/shop.jsx
+++ frontend/src/pages/shop.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import Popular from '../components/Popular/Popular'
  const shop = () => {
   return (
     <div>
         <Hero />
+        <Popular/>
     </div>
   )
 }
